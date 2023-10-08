e80ee5374998874d3003d86df5e77d972d206f6b
Bana <Banashbib@gmail.com>
2023-10-08 12:21:49 +0300

    Initial Commit

---
+++ fapp/fappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fappApp.swift
+//  fapp
+//
+//  Created by Bana Alshabib on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct fappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
